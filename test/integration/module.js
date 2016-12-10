95dbe569226a890c19b1deacd2d15fbf392344c9
chrisguttandin <chrisguttandin@media-codings.com>
2016-12-10 03:45:04 +0100

    apply linting rules

--- test/integration/module.js
+++ test/integration/module.js
@@ -1,16 +1,16 @@
 'use strict';
 
-var CLIEngine = require('eslint').CLIEngine,
-    config = require('../../src/module.js');
+const CLIEngine = require('eslint').CLIEngine;
+const config = require('../../src/module.js');
 
 describe('eslint-config-holy-grail', () => {
 
-    var cli;
+    let cli;
 
     beforeEach(() => cli = new CLIEngine(config));
 
     it('should lint a dummy text', () => {
-        var report = cli.executeOnText("'use strict';", 'fake.txt');
+        const report = cli.executeOnText("'use strict';", 'fake.txt');
 
         expect(report.errorCount).to.equal(0);
     });
