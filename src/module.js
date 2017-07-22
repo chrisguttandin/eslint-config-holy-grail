e65e4fa0dde0258d0274c176c10d64b4885170fa
chrisguttandin <chrisguttandin@media-codings.com>
2017-07-22 22:42:58 +0200

    remove getter-return rule as it produces false positives

--- src/module.js
+++ src/module.js
@@ -17,7 +17,6 @@ module.exports = {
         'eqeqeq': 2,
         'for-direction': 2,
         'func-call-spacing': 2,
-        'getter-return': 2,
         'indent': [ 2, 4, { VariableDeclarator: { let: 1, var: 1 } } ],
         'line-comment-position': [ 2, { position: 'above' } ],
         'multiline-ternary': [ 2, 'always-multiline' ],
