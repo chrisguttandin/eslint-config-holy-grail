89b3fa26270d988d52deb45bdc7ec6f0b9141438
Christoph Guttandin <chrisguttandin@media-codings.com>
2018-07-17 13:51:42 +0200

    feat: add node/prefer-global/url-search-params rule

--- src/module.js
+++ src/module.js
@@ -90,6 +90,7 @@ module.exports = {
         'node/prefer-global/console': [ 'error' ],
         'node/prefer-global/process': [ 'error', 'never' ],
         'node/prefer-global/url': [ 'error' ],
+        'node/prefer-global/url-search-params': [ 'error' ],
         'object-curly-newline': [ 'error', { consistent: true } ],
         'object-curly-spacing': [ 'error', 'always' ],
         'object-property-newline': [ 'error', { allowAllPropertiesOnSameLine: true } ],
