2cdade057e03b01cb32e8d2e7c45959ab48a251d
Christoph Guttandin <chrisguttandin@media-codings.com>
2022-01-18 00:30:39 +0100

    feat: add @angular-eslint/prefer-on-push-component-change-detection rule

--- src/module.js
+++ src/module.js
@@ -30,6 +30,7 @@ module.exports = {
                 '@angular-eslint/no-outputs-metadata-property': 'error',
                 '@angular-eslint/no-pipe-impure': 'error',
                 '@angular-eslint/no-queries-metadata-property': 'error',
+                '@angular-eslint/prefer-on-push-component-change-detection': 'error',
                 '@angular-eslint/relative-url-prefix': 'error',
                 '@angular-eslint/use-component-view-encapsulation': 'error',
                 '@angular-eslint/use-injectable-provided-in': 'error',
